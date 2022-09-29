f3ce9a4530c16b01b86aa54984563d25603328f4
Metehan Gültekin <mgmetehan@hotmail.com>
2022-09-29 14:09:40 +0300

    Add base project

---
+++ history-tracking/src/main/java/com/mgmetehan/historytracking/HistoryTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.historytracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryTrackingApplication.class, args);
+	}
+
+}
---
+++ history-tracking/src/test/java/com/mgmetehan/historytracking/HistoryTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.historytracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
